6caaab85210f7deb367bac89b64ab47d4c9b6fcb
Raheem <raheemideadev@gmail.com>
2024-04-25 22:04:14 +0500

    Installed dependencies;

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "xyz.teamgravity.animateddialog"
+    compileSdk = libs.versions.sdk.compile.get().toInt()
+
+    defaultConfig {
+        applicationId = "xyz.teamgravity.animateddialog"
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.java.target.get()
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // compose
+    implementation(platform(libs.compose))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.preview)
+    implementation(libs.compose.material3)
+
+    // compose activity
+    implementation(libs.compose.activity)
+
+    // compose lifecycle
+    implementation(libs.compose.lifecycle)
+
+    // core
+    implementation(libs.core)
+}
